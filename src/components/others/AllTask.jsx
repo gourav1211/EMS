a983b31d9b298688d5d60c6f92edf1c1e8dbf1a6
K Gourav <k.gourav254@gmail.com>
2024-12-22 00:05:06 +0530

    admin page data setup

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -1,38 +1,33 @@
-import React, { useContext } from 'react'
-import { AuthContext } from '../../context/AuthProvider'
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
 
 const AllTask = () => {
-
-    const authData = useContext(AuthContext)
+  const authData = useContext(AuthContext);
 
   return (
-    <div className='bg-black p-5 mt-5 rounded overflow-auto h-44'>
-        <div className='bg-red-400 py-2 px-4 flex justify-between mb-2'>
-            <h2>Gourav</h2>
-            <h3>Make a UI</h3>
-            <h5>Status</h5>
-        </div>
-
-        <div className='bg-yellow-400 py-2 px-4 flex justify-between mb-2'>
-            <h2>Gourav</h2> 
-            <h3>Make a UI</h3>
-            <h5>Status</h5>
-        </div>
-
-        <div className='bg-blue-400 py-2 px-4 flex justify-between mb-2'>
-            <h2>Gourav</h2>
-            <h3>Make a UI</h3>
-            <h5>Status</h5>
-        </div>
-
-        <div className='bg-green-400 py-2 px-4 flex justify-between mb-2'>
-            <h2>Gourav</h2>
-            <h3>Make a UI</h3>
-            <h5>Status</h5>
-        </div>
-      
+    <div className="bg-black p-5 mt-5 rounded">
+      <div className="bg-red-400 py-2 px-4 flex justify-between mb-2">
+        <h2 className="text-lg font-medium w-1/5">Employee Name</h2>
+        <h3 className="text-lg font-medium w-1/5">New Task</h3>
+        <h5 className="text-lg font-medium w-1/5">Active Task</h5>
+        <h5 className="text-lg font-medium w-1/5">Completed</h5>
+        <h5 className="text-lg font-medium w-1/5">Failed</h5>
+      </div>
+      <div className="overflow-auto">
+        {authData.employees.map((elem) => {
+          return (
+            <div className=" border-2 border-emerald-500 py-2 px-4 flex justify-between mb-2">
+              <h2 className="text-lg font-medium w-1/5 text-red-500">{elem.firstName}</h2>
+              <h3 className="text-lg font-medium w-1/5 text-blue-500">{elem.taskNumbers.newTask }</h3>
+              <h5 className="text-lg font-medium w-1/5 text-yellow-500">{elem.taskNumbers.active }</h5>
+              <h5 className="text-lg font-medium w-1/5 text-white">{elem.taskNumbers.completed }</h5>
+              <h5 className="text-lg font-medium w-1/5 text-red-500">{elem.taskNumbers.failed }</h5>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllTask
+export default AllTask;
